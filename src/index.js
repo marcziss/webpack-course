8ddc62643396105d71f0cc6c344ad63e7564aef4
marcziss <mziss@hotmail.com>
2020-06-18 00:19:40 -0400

    Changes made to try to add ckeditor

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './basePath.js';
+import 'ckeditor4';
+
+// Insert your code here.
+// The code below is an example of using CKEditor 4.
+CKEDITOR.replace( 'app', {
+    extraPlugins: 'iframe'
+} );
